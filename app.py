bf164266c116ef2b0619f2522ff6bc62f6a20367
Arip Zainul <aripzainul16@gmail.com>
2022-02-24 15:15:25 +0700

    hide style streamlit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 st.set_page_config(
-   page_title="Streamlit Test", #ini header dalam websitenya
+   page_title="Azazin", #ini header dalam websitenya
    #link pavicon : https://www.webfx.com/tools/emoji-cheat-sheet/
    page_icon=":octocat:", #ini untuk pavicon
    layout="wide"
@@ -118,3 +118,19 @@ col1, col2, col3 = st.columns(3)
 col1.metric("Temperature", "70 °F", "1.2 °F")
 col2.metric("Wind", "9 mph", "-8%")
 col3.metric("Humidity", "86%", "4%")
+
+
+
+
+
+
+
+# ----- HIDE STREAMLIT STYLE -----
+hide_st_style = """
+   <style>
+   #MainMenu {visibility: hidden;}
+   footer {visibility: hidden;}
+   header {visibility: hidden;}
+   </style>
+   """
+st.markdown(hide_st_style, unsafe_allow_html=True)
